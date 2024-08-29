c9566c3efb236c836e09e8d57b644a984f20a622
Jerome <jrah@itu.dk>
2024-08-29 11:52:13 +0200

    biiiig progress halvvejs igennem intro2 1.2

--- Intro/Intro2.fs
+++ Intro/Intro2.fs
@@ -20,10 +20,13 @@ let cvalue = lookup env "c";;
 
 (* Object language expressions with variables *)
 
+
 type expr = 
   | CstI of int
   | Var of string
-  | Prim of string * expr * expr;;
+  | Prim of string * expr * expr
+  | If of expr * expr * expr
+
 
 let e1 = CstI 17;;
 
@@ -31,19 +34,66 @@ let e2 = Prim("+", CstI 3, Var "a");;
 
 let e3 = Prim("+", Prim("*", Var "b", CstI 9), Var "a");;
 
+let e4 = Prim("max", CstI 3, CstI 2);;
+
+let e5 = Prim("min", CstI 3, CstI 2);;
+
+let e6 = Prim("==", CstI 3, CstI 2);;
+
+
 
 (* Evaluation within an environment *)
 
 let rec eval e (env : (string * int) list) : int =
     match e with
-    | CstI i            -> i
-    | Var x             -> lookup env x 
-    | Prim("+", e1, e2) -> eval e1 env + eval e2 env
+    | CstI i-> i
+    | Var x -> lookup env x 
+    | If(e1, e2, e3 ) ->
+      let u1 = eval e1 env
+      match u1 with 
+      |0 -> eval e2 env
+      |_ -> eval e3 env
+    | Prim(ope, e1, e2) -> 
+      let i1 = eval e1 env 
+      let i2 = eval e2 env
+      match ope with 
+      | "+" -> i1 + i2
+      | "-" -> i1 - i2
+      | "*" -> i1 * i2 
+      | "max" -> if i1 > i2 then i1 else i2 
+      | "min" -> if i1 < i2 then i1 else i2
+      | "==" -> if i1 = i2 then 1 else 0 
+      | _ -> failwith "unknown";;
+
+(*
     | Prim("*", e1, e2) -> eval e1 env * eval e2 env
     | Prim("-", e1, e2) -> eval e1 env - eval e2 env
+    | Prim("max",e1, e2) -> if eval e1 env > eval e2 env then eval e1 env else eval e2 env    
+    | Prim("min",e1, e2) -> if eval e1 env < eval e2 env then eval e1 env else eval e2 env 
+    | Prim("==",e1, e2) -> if eval e1 env = eval e2 env then 1 else 0       
     | Prim _            -> failwith "unknown primitive";;
+*)
+
 
 let e1v  = eval e1 env;;
 let e2v1 = eval e2 env;;
 let e2v2 = eval e2 [("a", 314)];;
 let e3v  = eval e3 env;;
+
+let e4v = eval e4 env;; 
+let e5v = eval e5 [("t", 200)];; 
+let e6v = eval e6 env;; 
+
+
+
+(* AEXPRESSIONS FROM HERE *)
+
+type aexpr =
+  | CstI of int
+  | Var of string
+  | Add of aexpr * aexpr
+  | Mul of aexpr * aexpr
+  | Sub of aexpr * aexpr
+
+let ae1 = Sub(Var "v", Add(Var "w", Var "z"))
+let ae2 = Mul(CstI 2, Sub(Var "v",(Add(Var "w", Var "z")))) 
