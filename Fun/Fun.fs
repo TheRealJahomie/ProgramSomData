0b8d8cdbc9fe98f7f4943d6b97903593c42eed54
Jerome <jrah@itu.dk>
2024-09-26 10:16:52 +0200

    working on 4.4

--- Fun/Fun.fs
+++ Fun/Fun.fs
@@ -24,46 +24,52 @@ let rec lookup env x =
 
 type value = 
   | Int of int
-  | Closure of string * string * expr * value env       (* (f, x, fBody, fDeclEnv) *)
+  | Closure of string * string list * expr * value env       (* (f, x, fBody, fDeclEnv) *)
 
 let rec eval (e : expr) (env : value env) : int =
     match e with 
     | CstI i -> i
     | CstB b -> if b then 1 else 0
     | Var x  ->
-      match lookup env x with
-      | Int i -> i 
-      | _     -> failwith "eval Var"
+        match lookup env x with
+        | Int i -> i 
+        | _     -> failwith "eval Var: not an integer"
     | Prim(ope, e1, e2) -> 
-      let i1 = eval e1 env
-      let i2 = eval e2 env
-      match ope with
-      | "*" -> i1 * i2
-      | "+" -> i1 + i2
-      | "-" -> i1 - i2
-      | "=" -> if i1 = i2 then 1 else 0
-      | "<" -> if i1 < i2 then 1 else 0
-      | _   -> failwith ("unknown primitive " + ope)
+        let i1 = eval e1 env
+        let i2 = eval e2 env
+        match ope with
+        | "*" -> i1 * i2
+        | "+" -> i1 + i2
+        | "-" -> i1 - i2
+        | "=" -> if i1 = i2 then 1 else 0
+        | "<" -> if i1 < i2 then 1 else 0
+        | _   -> failwith ("unknown primitive " + ope)
     | Let(x, eRhs, letBody) -> 
-      let xVal = Int(eval eRhs env)
-      let bodyEnv = (x, xVal) :: env
-      eval letBody bodyEnv
+        let xVal = Int(eval eRhs env)
+        let bodyEnv = (x, xVal) :: env
+        eval letBody bodyEnv
     | If(e1, e2, e3) -> 
-      let b = eval e1 env
-      if b<>0 then eval e2 env
-      else eval e3 env
-    | Letfun(f, x, fBody, letBody) -> 
-      let bodyEnv = (f, Closure(f, x, fBody, env)) :: env 
-      eval letBody bodyEnv
-    | Call(Var f, eArg) -> 
-      let fClosure = lookup env f
-      match fClosure with
-      | Closure (f, x, fBody, fDeclEnv) ->
-        let xVal = Int(eval eArg env)
-        let fBodyEnv = (x, xVal) :: (f, fClosure) :: fDeclEnv
-        eval fBody fBodyEnv
-      | _ -> failwith "eval Call: not a function"
+        let b = eval e1 env
+        if b <> 0 then eval e2 env
+        else eval e3 env
+    | Letfun(f, xs, fBody, letBody) -> 
+        let fClosure = Closure(f, xs, fBody, env)
+        let bodyEnv = (f, fClosure) :: env
+        eval letBody bodyEnv
+    | Call(Var f, eArgs) -> 
+        let fClosure = lookup env f
+        match fClosure with
+        | Closure (f, xs, fBody, fDeclEnv) ->
+            if List.length xs <> List.length eArgs then
+                failwith "eval Call: argument count mismatch"
+            else
+                let xVals = List.map (fun e -> Int(eval e env)) eArgs
+                let paramsAndArgs = List.zip xs xVals
+                let fBodyEnv = paramsAndArgs @ ((f, fClosure) :: fDeclEnv)
+                eval fBody fBodyEnv
+        | _ -> failwith "eval Call: not a function"
     | Call _ -> failwith "eval Call: not first-order function"
+
 
 (* Evaluate in empty environment: program must have no free variables: *)
 
@@ -71,7 +77,7 @@ let run e = eval e [];;
 
 (* Examples in abstract syntax *)
 
-let ex1 = Letfun("f1", "x", Prim("+", Var "x", CstI 1), 
+let ex1 = Letfun("f1", ["x"], Prim("+", Var "x", CstI 1), 
                  Call(Var "f1", CstI 12));;
 
 (* Example: factorial *)
